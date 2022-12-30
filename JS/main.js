d4b2a3dbdf41358be8357df2cf44ee98c8acd026
abdiaziz2112 <maashaaabdiaziz@gmail.com>
2022-12-30 15:50:14 +0300

    ver3

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,6 @@ let burgerEl = document.querySelector(".burger");
 let navigation_barEl = document.querySelector(".navigation-bar");
 
 burgerEl.addEventListener("click", ()=>{
-
     /* console.log("yes"); */
     navigation_barEl.classList.toggle("active")
 })
