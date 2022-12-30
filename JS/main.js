171631d2084dd54cdb06a8439b573ca0455bdd1b
abdiaziz2112 <maashaaabdiaziz@gmail.com>
2022-12-30 21:08:11 +0300

    ver4

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,9 @@ let navigation_barEl = document.querySelector(".navigation-bar");
 
 burgerEl.addEventListener("click", ()=>{
     /* console.log("yes"); */
+ // toggle if navigations appears hide otherwise display it
     navigation_barEl.classList.toggle("active")
 })
+
+
 
